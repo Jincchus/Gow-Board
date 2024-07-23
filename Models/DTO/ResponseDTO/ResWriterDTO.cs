dca4ca479597ef29283b181434bd3b8c42b4db91
Jincchus <chopoo2001@gmail.com>
2024-07-23 15:57:25 +0900

    repository 정리

--- Models/DTO/ResponseDTO/ResWriterDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GowBoard.Models.DTO.ResponseDTO
-{
-    public class ResWriterDTO
-    {
-        public string MemberId { get; set; }
-        public string Nickname { get; set; }
-    }
-}
